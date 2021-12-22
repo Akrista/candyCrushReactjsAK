7e80bf22f22ce9b74375c0e7de7874fd4bad90be
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2021-12-22 16:46:14 -0400

    Init bro

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import { useState, useEffect } from "react";
+const width = 8;
+const candyColors = ["red", "blue", "green", "yellow", "orange", "purple"];
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+      if (
+        columnOfFour.every(
+          (square) => currentColorArrangement[square === decidedColor]
+        )
+      ) {
+        columnOfFour.forEach(
+          square => currentColorArrangement[square] = ""
+        );
+      }
+    }
+  };
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square === decidedColor]
+        )
+      ) {
+        columnOfThree.forEach(
+          square => currentColorArrangement[square] = ""
+        );
+      }
+    }
+  };
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangement[square === decidedColor]
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForColumnOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
+
+  console.log(currentColorArrangement);
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
